4706cd55cd7978304da54552201e0ac89ff71289
Zé Bateira <jlageb+github@gmail.com>
2013-12-07 19:37:52 +0000

    Initial commit.
    Hello world and pyramid added.

---
+++ src/Pyramid.java
@@ -0,0 +1,16 @@
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		int counter = 1;
+		
+		for (int i = 0; i < 10; i++) {
+			counter++;
+			System.out.println();
+			for (int j = 1; j < counter; j++) {
+				System.out.print("*");
+			}
+		}
+		
+	}
+}
